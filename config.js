49fc40042b9111d52438816116af7e82885bced7
mahijano <157773647+mahijano@users.noreply.github.com>
2024-04-19 11:47:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,8 +17,8 @@ global.DATABASE_URI = process.env.DATABASE_URL || ""
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/mahijano";
 global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/CDWzBxhrB4uEmDEavQjT70";
-global.website=process.env.GURL || "https://chat.whatsapp.com/CDWzBxhrB4uEmDEavQjT70" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
+global.website=process.env.GURL || "https://whatsapp.com/channel/0029VaFv1RZ3QxRtLXfI993h" ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://chat.whatsapp.com/CDWzBxhrB4uEmDEavQjT70" ; // SET LOGO FOR IMAGE 
 
 
 
